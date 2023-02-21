0d63296ef06c95846080f562624cdd3cab9e943a
kkh061101 <kkh061101@naver.com>
2023-02-21 17:16:13 +0900

    메인 페이지 작성 시작 최근 소식 부분  & 수신 데이터 링크 화면 표시 성공 & 메인 페이지 내 로그인 및 회원가입 이동 버튼 추가

---
+++ src/Contents/Main.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import './Main.scss';
+
+function Main() {
+  const [id, setId] = useState([]);
+  useEffect(e => {
+    let data = ["https://youtu.be/V6TEcoNUmc8",
+      "https://youtu.be/7goHyFzym2I",
+      'https://youtu.be/_R0p00lv790',
+      'https://youtu.be/SK6Sm2Ki9tI',
+      'https://youtu.be/BG3A1P4Zmt4',
+      'https://youtu.be/N04Dyh8eNIk',
+      'https://youtu.be/BUfCW_cmCNQ',
+
+    ];
+    for (let i = 0; i < data.length; i++) {
+      data[i] = data[i].slice(-11);
+    }
+    setId(data);
+  }, []);
+  return <div className="main">
+    <Link to='/login'><button>Log in</button></Link>
+    <Link to='/signup'><button>Sign Up</button></Link>
+    <div className="Emptybox">
+
+    </div>
+    <div className="Updated">
+      <h3>최근 소식</h3>
+      <div className="contents">
+        {id.map((id, n) => <Box key={n} id={id} n={n} />)}
+      </div>
+    </div>
+  </div>;
+}
+
+function Box({ id, n }) {
+  const [data, setData] = useState([]);
+  useEffect(e => {
+    parsing();
+    // eslint-disable-next-line
+  }, []);
+  const parsing = async () => {
+    const URL_info = `https://noembed.com/embed?url=http://youtu.be/${id}`;
+    const data = await (await fetch(URL_info)).json();
+    setData(data);
+    console.log(data);
+  }
+  return <div className={`box ${n + 1}`} onClick={() => { window.open(`http://youtu.be/${id}`) }} title={data.title + ' 듣기'}>
+    <div className="text">
+      <h2>{n + 1}.</h2>
+      <p>&nbsp;<b>{data.title}</b></p>
+      <p>&nbsp;<b>{data.author_name}</b></p>
+    </div>
+    <div className="img">
+      <img src={data.thumbnail_url} alt="Thumbnail" />
+    </div>
+  </div>;
+}
+
+export default Main;
